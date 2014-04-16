36f5e0e7d0c9dbe9f2311d7faf9f32c8f481c27e
Igor Andrade <igorandrade@tokenlab.com.br>
2014-04-15 23:42:54 -0300

    Second commit

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -9,10 +9,32 @@
 #import "AttributorViewController.h"
 
 @interface AttributorViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *headline;
+@property (weak, nonatomic) IBOutlet UITextView *body;
 @end
 
 @implementation AttributorViewController
+
+- (IBAction)changeBodySelectionColorToMatchBackgroundButton:(UIButton *)sender
+{
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
+
+- (IBAction)outlineBodySelection
+{
+    [self.body.textStorage
+     addAttributes:@{ NSStrokeWidthAttributeName: @-3,
+                      NSStrokeColorAttributeName: [UIColor blackColor] }
+     range:self.body.selectedRange];
+}
+
+- (IBAction)unoutlineBodySelection
+{
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName
+                                     range:self.body.selectedRange];
+}
 
 - (void)viewDidLoad
 {
@@ -20,10 +42,10 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+//- (void)didReceiveMemoryWarning
+//{
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
 
 @end
